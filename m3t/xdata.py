306bb1945376cb5f21685841db62d64fea8516ec
LSCZS Macbook <reinaldo.lascazas@gmail.com>
2022-12-01 15:25:54 -0300

    initial_code

---
+++ m3t/xdata.py
@@ -0,0 +1,25 @@
+#######################################################################
+# Copyright (c) 2022-present, Las Cazas, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+######################################################################
+# Exploratory Data Analysis - M3T Project
+# 
+# xda: Making sense of data to drive insights.
+#
+# With XDA, exploring a dataset and finding insights is as 
+# easy as a few clicks and views. Using data to make it 
+# understandable for key business goals. Exploratory Data 
+# Analysis refers to the process of performing initial 
+# investigations on data to discover patterns, to spot 
+# anomalies, to test hypothesis and to check assumptions 
+# with the help of summary statistics and graphical representations.
+######################################################################
+
+import pandas as pd
+import numpy as np
+import seaborn as sn
+import matplotlib.pyplot as plt
+
